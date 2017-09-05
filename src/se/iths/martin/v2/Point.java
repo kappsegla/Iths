60a3e1a0e83d11ac91bb0768168b838b0261e85d
Martin Blomberg <martin.blomberg@outlook.com>
2017-09-05 13:44:26 +0200

    PointsDemo
    Kommentarer tillagda på Points klassen som förklarar den mesta koden.

--- src/se/iths/martin/v2/Point.java
+++ src/se/iths/martin/v2/Point.java
@@ -1,18 +1,26 @@
 package se.iths.martin.v2;
 
+//Deklaration av klassen Point som vi sedan
+//kan använda som mall för att skapa objekt av typen Point som kan lagra två koordinater, x och y.
 public class Point {
+    //Attribut eller fält som används för att lagra informationen.
+    //Bör vara privata och användas via getters/setters
     private int x;
     private int y;
 
+    //Default konstruktor. Använder constructor chaining för att skicka vidare till annan konstruktor.
     public Point() {
         this(0, 0);
     }
 
+    //Konstruktor för Point klassen som tar 2 argument. Sparar dessa i interna fälten.
     public Point(int x, int y) {
         this.setX(x);
         this.setY(y);
     }
 
+    //Metod som får en referens till ett annat point objekt som argument.
+    //Beräknar distansen mellan det egna objektet och det inskickade med hjälp av pythagoras sats.
     public double distanceTo(Point p) {
         int deltaX = this.getX() - p.getX();
         int deltaY = this.getY() - p.getY();
@@ -21,10 +29,11 @@ public class Point {
         return distance;
     }
 
+    //Getter metod för x-coordinaten
     public int getX() {
         return x;
     }
-
+    //Setter metod för x-coordinaten
     public void setX(int x) {
         this.x = x;
     }
@@ -37,6 +46,8 @@ public class Point {
         this.y = y;
     }
 
+    //equals är en metod där vi ersätter en redan färdig metod som finns på alla objekt i java.
+    //Har till uppgift att jämföra två objekt med varandra och returnera true om dessa är lika
     @Override
     public boolean equals(Object obj) {
         Point p1 = (Point) obj;
@@ -46,6 +57,8 @@ public class Point {
         return false;
     }
 
+    //Statisk metod main som låter oss starta Point klassen och testköra denna.
+    //Kommer inte påverka om vi skapar objekt av Point i andra applikationer.
     public static void main(String[] args) {
         Point p1 = new Point(1, 1);
         Point p2 = new Point(4, 5);
