1ca659e7d94d4fb8ef956b9d8f2a451f7d7b9e41
Martin Blomberg <martin.blomberg@outlook.com>
2017-09-12 15:29:14 +0200

    Lets deal

--- src/se/iths/martin/exercise3/Vehicle.java
+++ src/se/iths/martin/exercise3/Vehicle.java
@@ -3,6 +3,7 @@ package se.iths.martin.exercise3;
 public class Vehicle {
     private int weight;
     private int price;
+    private Person owner;
 
     public Vehicle(int weight, int price){
         this.weight = weight;
@@ -15,5 +16,13 @@ public class Vehicle {
 
     public int getWeight() {
         return weight;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
     }
 }
--- src/se/iths/martin/exercise3/VehicleDealer.java
+++ src/se/iths/martin/exercise3/VehicleDealer.java
@@ -4,4 +4,24 @@ public class VehicleDealer extends Person {
     public VehicleDealer(String name, String occupation, int age, int savings) {
         super(name, occupation, age, savings);
     }
+
+    public void vehicleTransaction(Buyer vehicleBuyer, Vehicle theVehicle){
+        Person vehicleSeller = theVehicle.getOwner();
+
+        //Collect money from buyer
+        int price = theVehicle.getPrice();
+        vehicleBuyer.setSavings( vehicleBuyer.getSavings() - price);
+
+        //Take our cut, 10%
+        int provision = (int)(price * 0.1);
+        setSavings( getSavings() + provision );
+
+        //Send money to seller
+        vehicleSeller.setSavings( vehicleSeller.getSavings() + price - provision );
+
+        //Change owner of car
+        theVehicle.setOwner(vehicleBuyer);
+
+
+    }
 }
